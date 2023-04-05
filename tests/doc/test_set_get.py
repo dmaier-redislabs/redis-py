62c44294c1ab8b5a4c15ce03155b4f6e9d2527e4
David Maier <60782329+dmaier-redislabs@users.noreply.github.com>
2023-04-05 21:17:25 +0200

    Create test_set_get.py
    First doc example

---
+++ tests/doc/test_set_get.py
@@ -0,0 +1,20 @@
+# HIDE_START
+import redis
+
+r = redis.Redis(
+    host="localhost",
+    port=6379,
+    db=0,
+    decode_responses=True
+)
+# HIDE_END
+
+status = r.set('bike:1', 'Process 134')
+
+if status == True:
+  print(Successfully added a bike.)
+
+value = r.get('bike:1')
+
+if value:
+  print('The name of the bike is: ' + value + '.')
