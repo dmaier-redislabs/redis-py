27a69ee8d9b702700bbd6f5df624f39c418ecc0e
David Maier <david.maier@redis.com>
2023-04-11 14:30:29 +0200

    Added test wrapper

--- tests/doc/test_set_get.py
+++ tests/doc/test_set_get.py
@@ -1,21 +1,33 @@
 # EXAMPLE: set_and_get
 # HIDE_START
 import redis
+# REMOVE_START
+import pytest
+# REMOVE_END
 
-r = redis.Redis(
+def run():
+  
+  r = redis.Redis(
     host="localhost",
     port=6379,
     db=0,
     decode_responses=True
-)
-# HIDE_END
+    )
+  # HIDE_END
+  
+  status = r.set('bike:1', 'Process 134')
+    
+  if status == True:
+    print('Successfully added a bike.')
+    value = r.get('bike:1')
+  
+  if value:
+    print('The name of the bike is: {}.'.format(value))
+  # REMOVE_START
+  assert True == status
+  assert 'Process 134' == value
 
-status = r.set('bike:1', 'Process 134')
-
-if status == True:
-  print('Successfully added a bike.')
-
-value = r.get('bike:1')
-
-if value:
-  print('The name of the bike is: {}.'.format(value))
+@pytest.mark.doc
+def test_run():
+  run()
+# REMOVE_END
